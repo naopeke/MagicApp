323de092af0a6b43f81b5c0f66632f8a6748e5d1
naopeke <naopeke@protonmail.com>
2024-03-04 20:59:01 +0100

    added grid

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { LoggedinCardComponent } from './pages/loggedin-card/loggedin-card.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent },
+  { path: 'card2', component: LoggedinCardComponent}
 ];
 
 @NgModule({
---
+++ src/app/pages/loggedin-card/loggedin-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-loggedin-card',
+  templateUrl: './loggedin-card.component.html',
+  styleUrls: ['./loggedin-card.component.css']
+})
+export class LoggedinCardComponent {
+
+}
