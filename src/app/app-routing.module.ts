089d08b0de3f458540e8e55563a935674a3cb3d6
naopeke <naopeke@protonmail.com>
2024-03-05 12:37:53 +0100

    cartas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
-import { LoggedinCardComponent } from './pages/loggedin-card/loggedin-card.component';
+import { CartasComponent } from './pages/cartas/cartas.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
   { path: 'home', component: HomeComponent },
-  { path: 'card2', component: LoggedinCardComponent}
+  { path: 'cartas', component: CartasComponent}
 ];
 
 @NgModule({
