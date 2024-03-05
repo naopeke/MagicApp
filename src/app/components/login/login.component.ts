64759267163c59b361d713c42e7df6c8204394b9
xisca-ayala <xisca.ayala@iMac-de-Antoni.bbrouter>
2024-03-05 13:04:18 +0100

    login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  public loginForm: FormGroup; 
+
+  constructor (private formBuilder: FormBuilder,
+    private router: Router){
+this.buildForm(); 
+}
+
+private buildForm(){
+  let minPassLength = 8;
+
+  this.loginForm = this.formBuilder.group({
+    email: [, [Validators.required, Validators.email]],
+    password:[, [Validators.required, Validators.minLength(minPassLength)]]
+  });
+}
+
+public login(){
+  
+}
+
+ngOnInit(): void {
+}
+
 
 }
