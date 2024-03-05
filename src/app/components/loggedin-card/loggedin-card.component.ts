61681f154531a1519b3a3ba7af73522d5d43cd9a
naopeke <naopeke@protonmail.com>
2024-03-05 21:15:10 +0100

    card

--- src/app/components/loggedin-card/loggedin-card.component.ts
+++ src/app/components/loggedin-card/loggedin-card.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router, Params } from '@angular/router';
+import { Card } from 'src/app/models/card';
+import { CardsService } from 'src/app/shared/cards.service';
 
 @Component({
   selector: 'app-loggedin-card',
@@ -6,5 +9,32 @@ import { Component } from '@angular/core';
   styleUrls: ['./loggedin-card.component.css']
 })
 export class LoggedinCardComponent {
+  public cards: Card[]
+  public card: Card;
+  public parametro: string;
 
+  constructor(
+    public cardsService: CardsService,
+    private router: Router,
+    private rutaActiva: ActivatedRoute
+  ){
+
+    //app.routing.module.ts  {path: "cartas/:cardId", component:CartasComponent},
+  this.parametro = this.rutaActiva.snapshot.params.cardId;
+  }
+
+  // buscar con las palabras completas
+  searchCards(searchParam: string): void {
+    let card = this.cardsService.getByName(searchParam);
+      if (card) {
+        console.log('Result: ', card);
+        this.router.navigate(['/cartas', card.id_card]);
+      } else {
+        console.log('No hay datos');
+      }
+  }
+
+  ngOnInit(): void {
+      
+  }
 }
