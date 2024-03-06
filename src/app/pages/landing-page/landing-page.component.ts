b7dc6101ed211bdfd0a761c726dfe1a855595f48
PatriciaGonFu <patruskatrus@hotmail.com>
2024-03-06 20:31:18 +0100

    landingAcabada

--- src/app/pages/landing-page/landing-page.component.ts
+++ src/app/pages/landing-page/landing-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-landing-page',
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./landing-page.component.css']
 })
 export class LandingPageComponent {
+
+  slidesPerView: number = 3;
+
+  screenWidth: number;
+  @HostListener ('window:resize')
+
+  getScreenWidth(){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth >= 320 && this.screenWidth <=480){
+      this.slidesPerView = 1
+    } else if(this.screenWidth >= 320 && this.screenWidth <=480){
+      this.slidesPerView = 2
+    } else  if(this.screenWidth >= 320 && this.screenWidth <=480){
+      this.slidesPerView = 3
+    }
+  }
 
 }
