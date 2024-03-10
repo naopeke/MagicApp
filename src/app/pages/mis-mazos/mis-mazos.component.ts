290572887ffc7365995d35be532524e65613992b
naopeke <naopeke@protonmail.com>
2024-03-10 20:49:16 +0100

    filter works. can show

--- src/app/pages/mis-mazos/mis-mazos.component.ts
+++ src/app/pages/mis-mazos/mis-mazos.component.ts
@@ -116,8 +116,12 @@ export class MisMazosComponent implements OnInit, AfterViewInit {
 
   public onFilterClick(typeLine:string){
     console.log('filtered creatures');
-    const filteredCards = this.mazo.cards.filter(c => c.type_line.toLowerCase().includes(typeLine.toLowerCase()));
-    console.log('filtered card', filteredCards);
+    if (typeLine === '' || typeLine === 'all'){
+      this.filteredCards = [...this.mazo.cards];
+    } else {
+      this.filteredCards = this.mazo.cards.filter(c => c.type_line.toLowerCase().includes(typeLine.toLowerCase()));
+    }
+    console.log('filtered card', this.filteredCards);
   }
 
 
