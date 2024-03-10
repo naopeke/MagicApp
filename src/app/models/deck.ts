176ecf150adc5f05b6394d49aa7d7e8d711ff969
naopeke <naopeke@protonmail.com>
2024-03-10 11:42:04 +0100

    temporal

--- src/app/models/deck.ts
+++ src/app/models/deck.ts
@@ -11,3 +11,14 @@ export class Deck {
 }
 
 // en realidad deberia ser el id_user, pero pongo name por ahora hasta BBDD
+
+// export class Deck {
+//     constructor(
+//         public id_deck?: number,
+//         public nameDeck?: string,
+//         public nameUser?: string,
+//         public scores?: number[], 
+//         public mediaScore?: number,
+//         public cards?: Card[],
+//        ){}
+// }
