904c217961b5052ff3234c87a0cab20d09923e2b
naopeke <naopeke@protonmail.com>
2024-04-01 18:37:44 +0200

    modificando service para vercel

--- src/app/shared/users.service.ts
+++ src/app/shared/users.service.ts
@@ -12,8 +12,9 @@ import { environment } from 'src/environments/environment';
 
 export class UsersService {
   public user:User;
-
-  private url = environment.url;
+  // private url = "http://localhost:3000/";
+  private url = "https://magy-deck-api.vercel.app/"; 
+  // private url = environment.url;
 
 // https://netbasal.com/angular-2-persist-your-login-status-with-behaviorsubject-45da9ec43243
   // si está logueado (true) o no (false) Manejar estado de Login
@@ -22,6 +23,8 @@ export class UsersService {
   // guarda info de users 
   private currentUserSubject = new BehaviorSubject<User | null>(null);
   public currentUser = this.currentUserSubject.asObservable();
+
+  // public loggedIn: boolean = true; 
 
 
   constructor(private http: HttpClient) { 
