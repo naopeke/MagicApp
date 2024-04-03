b31187e509a4e3f5ebc555ab19bcf681ddb9a4e3
naopeke <naopeke@protonmail.com>
2024-04-03 12:36:04 +0200

    header ok

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
 import { UsersService } from 'src/app/shared/users.service';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+
 
 @Component({
   selector: 'app-header',
@@ -14,11 +16,13 @@ export class HeaderComponent implements OnInit {
   public bandera:boolean;
   public modal_visible:boolean;
   public currentUser: User | null = null;
+  public isSmallScreen: boolean = false;
 
 
-  constructor(public usersService: UsersService, private router: Router){
+  constructor(public usersService: UsersService, private router: Router, private breakpointObserver: BreakpointObserver){
     this.modal_visible = false;
   }
+
 
   logOut() {
     this.modal_visible = false;
@@ -31,7 +35,7 @@ export class HeaderComponent implements OnInit {
     this.usersService.currentUserChanges().subscribe(user => {
       this.currentUser = user;
     });
-
+    this.breakpointObserver.observe([Breakpoints.XSmall])
+    .subscribe(result => this.isSmallScreen = result.matches);
 }
-
 }
