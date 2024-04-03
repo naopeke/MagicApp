bc1cd759082f9bf0438a568e77ccb3b75aca939a
Belen Castelli <mariabelencastelli29@gmail.com>
2024-04-03 17:06:44 +0200

    estrellas f

--- src/app/components/rating/rating.component.ts
+++ src/app/components/rating/rating.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Deck } from 'src/app/models/deck';
 
 @Component({
   selector: 'app-rating',
@@ -6,23 +7,24 @@ import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
   styleUrls: ['./rating.component.css']
 })
 export class RatingComponent implements OnInit {
-
+  @Input() mazo:Deck
   @Input() mediaScore: number;
   @Input () typeRating: number;
   @Output() onRating = new EventEmitter<number>()
 
-  public maxrating: number = 5;
-  public selectedStar:number = 0;
+
+  public selectedStarsMap: { [key: number]: number } = {}
   public showStar: number = 0
   public previoSelection:number = 0
-  // public maxRatingArr:any = []
-  public maxRatingArr = Array(this.maxrating).fill(0);
+  public maxRatingArr:number[] = [1,2,3,4,5]
+  // public maxRatingArr = Array(this.maxrating).fill(0);
+  // public maxRatingArr2 = Array(this.maxrating).fill(0);
+  // public maxRatingArr3 = Array(this.maxrating).fill(0);
 
   ngOnInit(): void {
-    // this.maxRatingArr = Array(this.maxrating).fill(0);
-    // console.log(this.maxRatingArr);
+ 
     
-    // console.log(this.selectedStar);
+    console.log(this.selectedStarsMap[this.mazo.id_deck]);
     
  
   }
@@ -34,17 +36,24 @@ export class RatingComponent implements OnInit {
     this.showStar = index +1
   }
   HandleMouseLeave(){
+    if(this.previoSelection != 0){
+      this.showStar = this.previoSelection
+    }else 
     this.showStar = 0
   }
   
-  rating(index:number){
-  this.selectedStar = index + 1;
-  console.log(this.selectedStar);
-  this.onRating.emit(this.selectedStar)
-  console.log(this.selectedStar);
+  rating(index:number, mazoId:number){
+    console.log(mazoId);
+    this.selectedStarsMap[mazoId] = index + 1; 
+    this.previoSelection = this.selectedStarsMap[mazoId];
+    console.log(this.previoSelection);
+    console.log(this.selectedStarsMap[mazoId]);
+    this.onRating.emit(this.selectedStarsMap[mazoId]);
+ 
   
   
   }
+
   roundMediaScore(){
     let resultado;
     if (this.mediaScore !== undefined) {
